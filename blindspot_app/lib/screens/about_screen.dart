dfcf5072d29fea82902c87a1fbacfcb787369755
xiaoxiaoliu2022 <98120990+xiaoxiaoliu2022@users.noreply.github.com>
2023-04-17 14:26:34 -0700

    fix layout of about page

--- blindspot_app/lib/screens/about_screen.dart
+++ blindspot_app/lib/screens/about_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../custom_widgets/custom_appbar.dart';
+
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
@@ -8,145 +10,137 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; //total size of our screen
     // ignore: sized_box_for_whitespace
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Image.asset(
-              "assets/images/top.jpeg",
-              width: size.width * 1,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const Positioned(
-            top: 50,
-            left: 0,
-            right: 0,
-            child: Text(
-              "BlindSpot",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.none,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const Positioned(
-            top: 130,
-            left: 0,
-            right: 0,
-            child: Text(
-              "Version 1.7",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.none,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 200),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          WidgetSpan(
-                            child: Icon(
-                              Icons.arrow_right,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Pick the topic of your choice and take the quiz\n",
-                            style: TextStyle(
-                              fontSize: 16,
-                              decoration: TextDecoration.none,
-                              fontFamily: 'Arial',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          WidgetSpan(
-                            child: Icon(
-                              Icons.arrow_right,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Star mark the question that you want to dive deeper into, to save it to your notebook\n",
-                            style: TextStyle(
-                              fontSize: 16,
-                              decoration: TextDecoration.none,
-                              fontFamily: 'Arial',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          WidgetSpan(
-                            child: Icon(
-                              Icons.arrow_right,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          TextSpan(
-                            text:
-                                "Visit your profile to see how much progress you’ve made and also personalize your profile using the setting button\n",
-                            style: TextStyle(
-                              fontSize: 16,
-                              decoration: TextDecoration.none,
-                              fontFamily: 'Arial',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 40),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text(
-                    "Copyright BlindSpot All Rights Reversed",
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 300,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: ReviseSize(),
+          child: Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.blue,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Center(
+                  heightFactor: 2,
+                  child: Text(
+                    "BlindSpot",
                     style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w100,
-                      fontSize: 12,
-                      decoration: TextDecoration.none,
+                      fontSize: 45,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                ],
-              ),
+                ),
+                const Text(
+                  "Version 2.0.0",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "All your coding quiz at one place",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.arrow_right,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    "Pick the topic of your choice and take the quiz",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Arial',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.arrow_right,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    "Star mark the question that you want to dive deeper into, to save it to your notebook",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Arial',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.arrow_right,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    "Visit your profile to see how much progress you’ve made and also personalize your profile using the setting button",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Arial',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 200),
+            const Text(
+              "© BlindSpot. All Rights Reversed",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w100,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ),
       ),
     );
   }
